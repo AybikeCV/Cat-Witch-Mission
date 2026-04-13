fcdc1fb7b15aff486903f065e5ebe5c4dfe56b0d
Aybike Celebi Visser <celebiaybike@gmail.com>
2026-04-13 20:56:19 +0200

    13thapril prog

---
+++ js/MainGame.js
@@ -0,0 +1,37 @@
+//*GLOBAL DOM ELEMENTS
+
+//Screens
+
+const startScreenNode = document.querySelector("#start-screen");
+const gameScreenNode = document.querySelector("#game-screen");
+const gameOverScreenNode = document.querySelector("#game-over-screen");
+
+//Buttons
+
+const startBtnNode = document.querySelector("#start-btn");
+const replayBtnNode = document.querySelector("#replay-btn");
+
+//Game-box
+
+const gameBoxNode = document.querySelector("#game-box");
+
+//*GLOBAL GAME VARIABLES
+
+
+
+
+//*GLOBAL GAME FUNCTIONS
+
+
+
+
+
+
+//*EVENT LISTENERS
+
+
+
+
+
+
+//*PLANNING
